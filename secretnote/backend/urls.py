20ee7148fa95389369ceb5efcb2724773757d927
fatmaebrahim <fatmaebrahim3131@gmail.com>
2024-10-09 16:12:22 +0300

    feat: added templates with no styles

--- secretnote/backend/urls.py
+++ secretnote/backend/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "notes"
 urlpatterns=[
     path('',views.index),
-    path('all/',views.allnotes),
+    path('all/',views.allnotes, name="all"),
+    path('add/', views.addnote,name="add"),
     path("<str:note_url>/",views.content,name="url"),
 ]
--- secretnote/backend/views.py
+++ secretnote/backend/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404,get_list_or_404
 
 # Create your views here.
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Note
+import uuid 
 
 
 def index(request):
@@ -17,4 +19,16 @@ def content(request, note_url):
 def allnotes(request):
     notes=get_list_or_404(Note)
     context={"notes": notes}
-    return render(request,"backend/index.html",context)
+    return render(request,"backend/allnotes.html",context)
+
+def addnote(request):
+    if request.method == "POST":
+        content=request.POST.get('content')
+        expiration=request.POST.get('expiration')  
+        views_limit=request.POST.get('views_limit')
+        n=Note(content=content,expiration=expiration,views_limit=views_limit,url=uuid.uuid4())
+        n.save()
+        print(n.created_at) #2024-10-09 12:53:10.248280+00:00  datetime
+        print(n.expiration) #2024-10-10T01:01 string
+        return HttpResponseRedirect(reverse("notes:all"))
+    return render(request,"backend/addnote.html")
